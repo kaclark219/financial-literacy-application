261418c1813e4f02c1a90648693582e92a0d6448
Katelyn Clark <kaclark219@gmail.com>
2024-11-17 17:22:39 -0600

    fixed custom goal pop-up fields, added ability to add custom goals

--- goals.js
+++ goals.js
@@ -39,3 +39,30 @@ document.getElementById("20-eating-out").addEventListener("click", () => {
 document.getElementById("no-unnecessary").addEventListener("click", () => {
     replaceGoalContent("Do not buy unnecessary things.", "1 Month+ | Spending", 1);
 });
+
+
+// custom goal
+document.getElementById("add-custom-goal").addEventListener("click", () => {
+    const description = document.querySelector("input[placeholder='Type here!']").value.trim();
+
+    const aspect = document.querySelector("input[name='subject']:checked")?.value;
+    const duration = document.querySelector("input[name='duration']:checked")?.value;
+    const priority = document.querySelector("input[name='priority']:checked")?.value;
+    if (!description || !aspect || !duration || !priority) {
+        alert("Please fill out all fields to add a goal!");
+        return;
+    }
+
+    let num_stars;
+    if (priority === "Low") {num_stars = 1;}
+    else if (priority === "Medium") {num_stars = 2;}
+    else if (priority === "High") {num_stars = 3;}
+
+    const formatted_duration =
+        duration === "OneWeek" ? "< 1 Week" :
+        duration === "OnetoThree" ? "1-3 Weeks" : "1 Month+";
+
+    const goal_details = `${formatted_duration} | ${aspect}`;
+
+    replaceGoalContent(description, goal_details, num_stars);
+});
